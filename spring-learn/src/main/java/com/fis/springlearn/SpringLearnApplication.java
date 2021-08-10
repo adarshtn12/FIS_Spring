3d981a1067a5b2bbe367a3bbe7b9e9c3dc44c70b
adarsh <adarshtnadhu1999@gmail.com>
2021-08-10 16:42:35 +0530

    added Multiple Bean using spring XML

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,38 +1,72 @@
 package com.fis.springlearn;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.fis.springlearn.bean.Employee;
+
+
 @SpringBootApplication
 public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
-		displayDate();
+//		displayDate();
+//		displayCountry();
+//		displayCountries();
+		displayEmployee();
 	}
 
+	static void displayEmployee() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+		context.getBean("employee",Employee.class);
+		Employee employee = context.getBean("employee", Employee.class);
+		LOGGER.debug("Employee : {}", employee);
+		LOGGER.info("END");
+	}
+
+//	static void displayCountries() {
+//		LOGGER.info("START");
+//		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+//		ArrayList<Country> countries = context.getBean("countryList", ArrayList.class);
+//		LOGGER.debug("Countries : {}", countries);
+//		LOGGER.info("END");
+//	}
+//	static void displayCountry() {
+//		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+//		Country country = context.getBean("country", Country.class);
+//		Country anotherCountry = context.getBean("country", Country.class);
+//		LOGGER.debug("Country : {}", country);
+//		LOGGER.debug("Country : {}", anotherCountry);
+//	}
 	static void displayDate() {
 		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 		try {
-			Date parseDate = format.parse("31/12/2020");
-			//System.out.println(parseDate);
-            LOGGER.debug(parseDate.toString());
+			Date parseDate = format.parse("31/12/2018");
+//			System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+//		SimpleDateFormat format1 = context.getBean("dateFormat1", SimpleDateFormat.class);
+//		try {
+//			Date parseDate1 = format1.parse("08/09/2021");
+////			System.out.println(parseDate1);
+//			LOGGER.debug(parseDate1.toString());
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
 		LOGGER.info("END");
-
 	}
-
 }
--- spring-learn/src/test/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/test/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,28 +1,39 @@
 package com.fis.springlearn;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.fis.springlearn.bean.Employee;
+
+
 @SpringBootApplication
 public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
-
-	public static void main(String[] args) {
-//		SpringApplication.run(SpringLearnApplication.class, args);
+ public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
 //		displayDate();
 //		displayCountry();
-		displayCountries();
+//		displayCountries();
+		displayEmployee();
 	}
-	
+
+	static void displayEmployee() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+		context.getBean("employee",Employee.class);
+		Employee employee = context.getBean("employee", Employee.class);
+		LOGGER.debug("Employee : {}", employee);
+		LOGGER.info("END");
+	}
+
 	static void displayCountries() {
 		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
@@ -30,16 +41,13 @@ public class SpringLearnApplication {
 		LOGGER.debug("Countries : {}", countries);
 		LOGGER.info("END");
 	}
-
 	static void displayCountry() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
 		Country country = context.getBean("country", Country.class);
 		Country anotherCountry = context.getBean("country", Country.class);
-		LOGGER.debug("Country : {}", country.toString());
+		LOGGER.debug("Country : {}", country);
 		LOGGER.debug("Country : {}", anotherCountry);
-
 	}
-
 	static void displayDate() {
 		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
@@ -51,16 +59,14 @@ public class SpringLearnApplication {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
 //		SimpleDateFormat format1 = context.getBean("dateFormat1", SimpleDateFormat.class);
 //		try {
 //			Date parseDate1 = format1.parse("08/09/2021");
-//			System.out.println(parseDate1);
+////			System.out.println(parseDate1);
 //			LOGGER.debug(parseDate1.toString());
 //		} catch (ParseException e) {
 //			e.printStackTrace();
 //		}
 		LOGGER.info("END");
 	}
-
 }
