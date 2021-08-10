408646dcbfe0d5edb5fc1d6a9230be990f6cb217
adarsh <adarshtnadhu1999@gmail.com>
2021-08-10 11:33:07 +0530

    added my first maven web project

--- spring-learn/src/test/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/test/java/com/fis/springlearn/SpringLearnApplication.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +14,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @SpringBootApplication
 public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		displayDate();
+		displayCountry();
+	}
+
+	static void displayCountry() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+
 	}
 
 	static void displayDate() {
@@ -25,14 +33,22 @@ public class SpringLearnApplication {
 		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 		try {
-			Date parseDate = format.parse("31/12/2020");
-			//System.out.println(parseDate);
-            LOGGER.debug(parseDate.toString());
+			Date parseDate = format.parse("31/12/2018");
+//			System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		SimpleDateFormat format1 = context.getBean("dateFormat1", SimpleDateFormat.class);
+		try {
+			Date parseDate1 = format1.parse("08/09/2021");
+//			System.out.println(parseDate1);
+			LOGGER.debug(parseDate1.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		LOGGER.info("END");
-
 	}
 
 }
