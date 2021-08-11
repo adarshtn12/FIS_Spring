56733246b3cce99ab864985eb11a976df834bcfe
adarsh <adarshtnadhu1999@gmail.com>
2021-08-11 17:24:45 +0530

    added employee spring JDBC

---
+++ spring-jdbc/src/main/java/com/fis/springjdbc/employee/TestEmployee.java
@@ -0,0 +1,40 @@
+package com.fis.springjdbc.employee;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class TestEmployee {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("daoexample.xml");
+		EmployeeDAOImpl bean = context.getBean("employeeDao", EmployeeDAOImpl.class);
+
+		System.out.println("===============================");
+		System.out.println("Insert a Employee");
+		Employee newEmployee = new Employee();
+		newEmployee.setId(30);
+		newEmployee.setName("adarsh");
+		bean.insertEmployee(newEmployee);
+		System.out.println("===============================");
+		System.out.println("Delete Employee");
+		int id = 10;
+		bean.deleteEmployee(id);
+		System.out.println("===============================");
+		System.out.println("Get Employee Based on Id");
+		id = 20;
+		Employee employeeById = bean.getEmployeeById(id);
+		System.out.println(employeeById);
+		System.out.println("===============================");
+		System.out.println("Update Employee");
+		newEmployee = new Employee();
+		newEmployee.setId(30);
+		newEmployee.setName("adarshtn");
+		bean.updateEmployee(newEmployee);
+		System.out.println("===============================");
+		System.out.println("Get All Employees");
+		System.out.println(bean.getAllEmployees());
+		System.out.println("===============================");
+
+		context.close();
+	}
+
+}
